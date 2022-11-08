3ac299b9b9a27613fc55c8f38659d31efe4eba25
fantengteng <593886467@qq.com>
2022-11-08 10:09:15 +0800

    初始化

---
+++ lib/hzy_normal_http_config.dart
@@ -0,0 +1,39 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: TT
+ * @Date: 2022-11-08 10:08:29
+ * @LastEditors: TT
+ * @LastEditTime: 2022-11-08 10:08:29
+ */
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: TT
+ * @Date: 2022-11-02 16:17:29
+ * @LastEditors: TT
+ * @LastEditTime: 2022-11-02 16:18:35
+ */
+
+import 'package:dio/dio.dart';
+
+/// dio 配置项
+class HzyNormalHttpConfig {
+  final String? baseUrl;
+  final String? proxy;
+  final String? cookiesPath;
+  final List<Interceptor>? interceptors;
+  final int connectTimeout;
+  final int sendTimeout;
+  final int receiveTimeout;
+
+  HzyNormalHttpConfig({
+    this.baseUrl,
+    this.proxy,
+    this.cookiesPath,
+    this.interceptors,
+    this.connectTimeout = 10000,
+    this.sendTimeout = 10000,
+    this.receiveTimeout = 10000,
+  });
+}
