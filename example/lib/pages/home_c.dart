db5fa4fdbe31ef7c6ed98d08d57448a142a667a2
fantengteng <593886467@qq.com>
2022-11-09 10:58:47 +0800

    提交哦

--- example/lib/pages/home_c.dart
+++ example/lib/pages/home_c.dart
@@ -4,12 +4,10 @@
  * @Author: TT
  * @Date: 2022-11-08 11:41:42
  * @LastEditors: TT
- * @LastEditTime: 2022-11-08 11:55:06
+ * @LastEditTime: 2022-11-09 09:43:30
  */
 
 import 'package:example/utils/getx_util_tool.dart';
-import 'package:hzy_normal_network/hzy_normal_client.dart';
-import 'package:hzy_normal_network/hzy_normal_http_config.dart';
 import 'package:hzy_normal_widget/hzy_normal_widget.dart';
 
 import '../components/common_index.dart';
@@ -17,9 +15,12 @@ import '../components/common_index.dart';
 class HomeC extends CommonGetXController {
   var text = "点我啊".obs;
 
-  void tapbutton() {
-    request(
+  void tapbutton() async {
+    var res = await request(
         path: "Work/GetChuFangOrderInfo",
         data: {"menZhenH": "MZ20221018-00001-0006"});
+    if (res.ok) {
+      debugprint(res.data);
+    } else {}
   }
 }
